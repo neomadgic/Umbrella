081bb981a44621c8312fd4a2dfd86db905c00b2a
Vu Dang <jublusion5@gmail.com>
2016-12-08 16:15:25 -0600

    Added Extensions folder along with the 2 swift files that the Nerdery made. Cleaned up AppDelegate

---
+++ Umbrella/String+Umbrella.swift
@@ -0,0 +1,34 @@
+//
+//  NSURL+Umbrella.swift
+//  Umbrella
+//
+//  Created by Jon Rexeisen on 10/13/15.
+//  Copyright © 2015 The Nerdery. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    /**
+    Returns the URL for the images to be used within the application
+    
+    - parameter highlighted: If the image is filled in or not
+    
+    - returns: The URL to be used within the application
+    */
+    func nrd_weatherIconURL(highlighted: Bool = false) -> URL? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "nerdery-umbrella.s3.amazonaws.com"
+        
+        if highlighted {
+            urlComponents.path = "/\(self)-selected.png"
+        } else {
+            urlComponents.path = "/\(self).png"
+        }
+        
+        return urlComponents.url
+    }
+    
+}

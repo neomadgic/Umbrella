4f3932a6213822da4c7924261f51d2ade6041e92
Vu Dang <jublusion5@gmail.com>
2016-12-08 16:08:28 -0600

    created UILabel classes for labels that will later be used in ViewController

---
+++ Umbrella/CurrentForecastLabel.swift
@@ -0,0 +1,21 @@
+//
+//  CurrentForecastLabel.swift
+//  Umbrella
+//
+//  Created by Vu Dang on 12/8/16.
+//  Copyright © 2016 The Nerdery. All rights reserved.
+//
+
+import UIKit
+
+class CurrentForecastLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Umbrella/TempForecastLabel.swift
@@ -0,0 +1,21 @@
+//
+//  TempForecastLabel.swift
+//  Umbrella
+//
+//  Created by Vu Dang on 12/8/16.
+//  Copyright © 2016 The Nerdery. All rights reserved.
+//
+
+import UIKit
+
+class TempForecastLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

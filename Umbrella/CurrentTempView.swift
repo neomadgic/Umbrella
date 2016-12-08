8a4fa1686326386e0381a74efb3200fb9a0e7809
Vu Dang <jublusion5@gmail.com>
2016-12-08 16:10:04 -0600

    Moved ViewController to Controller folder and added CurrentTempView to View Folder

---
+++ Umbrella/CurrentTempView.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentTempView.swift
+//  Umbrella
+//
+//  Created by Vu Dang on 12/8/16.
+//  Copyright © 2016 The Nerdery. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CurrentTempView: UIView {
+    
+    override func awakeFromNib() {
+        
+        backgroundColor = UIColor.red
+    }
+    
+}

bc76f61a9c5a7f2043b9d9bda3d08696c86d6b07
Vu Dang <jublusion5@gmail.com>
2016-12-09 11:31:29 -0600

    Deleted Scroll View and adjusted the UI on Collection View. Pinned it to storyboard

---
+++ Umbrella/HeaderColorView.swift
@@ -0,0 +1,19 @@
+//
+//  HeaderColorView.swift
+//  Umbrella
+//
+//  Created by Vu Dang on 12/9/16.
+//  Copyright © 2016 Vu Dang. All rights reserved.
+//
+
+import UIKit
+
+class HeaderColorView: UIView {
+
+    override func awakeFromNib() {
+        
+        backgroundColor = headerColor
+        
+    }
+
+}

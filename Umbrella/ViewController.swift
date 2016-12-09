67544669325caa4df5d2a798463eed19ae697a3d
Vu Dang <jublusion5@gmail.com>
2016-12-09 12:17:18 -0600

    Connected the UICollectionView to the code

--- Umbrella/ViewController.swift
+++ Umbrella/ViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +23,39 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+//MARK: - UICollectionViewDataSource
+extension ViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WeatherCell", for: indexPath) as! WeatherCell
+        cell.backgroundColor = UIColor.black
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        switch kind {
+        //2
+        case UICollectionElementKindSectionHeader:
+            //3
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                             withReuseIdentifier: "WeatherHeaderView",
+                                                                             for: indexPath) as! WeatherHeaderView
+            headerView.DayHeaderLbl.text = "Today"
+            return headerView
+        default:
+            //4
+            assert(false, "Unexpected element kind")
+        }
+    }
 }
---
+++ Umbrella/WeatherHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherHeaderView.swift
+//  Umbrella
+//
+//  Created by Vu Dang on 12/9/16.
+//  Copyright © 2016 Vu Dang. All rights reserved.
+//
+
+import UIKit
+
+class WeatherHeaderView: UICollectionReusableView {
+        
+    @IBOutlet weak var DayHeaderLbl: DayHeaderLabel!
+}

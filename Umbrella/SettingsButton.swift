a35ba2f514f8213736a66f13990757790e9725cc
Vu Dang <jublusion5@gmail.com>
2016-12-08 18:19:11 -0600

    Adjusted the UI of the CurrentTempertureView

---
+++ Umbrella/SettingsButton.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsButton.swift
+//  Umbrella
+//
+//  Created by Vu Dang on 12/8/16.
+//  Copyright © 2016 Vu Dang. All rights reserved.
+//
+
+import UIKit
+
+class SettingsButton: UIButton {
+
+    override func awakeFromNib() {
+        imageView!.contentMode = .scaleAspectFill
+    }
+
+}

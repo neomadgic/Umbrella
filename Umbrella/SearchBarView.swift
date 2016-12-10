3704568d7dec14e59551a8b0947bb60f2fdd0e78
Vu Dang <jublusion5@gmail.com>
2016-12-09 18:24:47 -0600

    Implemented custom search bar in SettingsVC. Created a SearchBarView and configured it

---
+++ Umbrella/SearchBarView.swift
@@ -0,0 +1,19 @@
+//
+//  SearchBarView.swift
+//  Umbrella
+//
+//  Created by Vu Dang on 12/9/16.
+//  Copyright © 2016 Vu Dang. All rights reserved.
+//
+
+import UIKit
+
+class SearchBarView: UIView {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 10.0
+        clipsToBounds = true
+        
+    }
+
+}
---
+++ Umbrella/SettingsVC.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsVC.swift
+//  Umbrella
+//
+//  Created by Vu Dang on 12/9/16.
+//  Copyright © 2016 Vu Dang. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
